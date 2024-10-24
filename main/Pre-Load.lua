0555499cac1829edf6d46c3f8c5b4479f01112be
Okho <109078241+NOOBARMYSCRIPTER@users.noreply.github.com>
2024-10-24 20:47:41 +0600

    Update Pre-Load.lua

--- main/Pre-Load.lua
+++ main/Pre-Load.lua
@@ -1,3 +1,6 @@
+if game.GameId == 2160907981 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/NOOBARMYSCRIPTER/SirHub/refs/heads/main/main/Scripts/SprayPaintPreLoad.lua"))()
+end
 local KeySystemUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/MaGiXxScripter0/keysystemv2api/master/ui/xrer_mstudio45.lua"))()
 KeySystemUI.New({
     ApplicationName = "Xrer",                                                                                            -- Your Key System
